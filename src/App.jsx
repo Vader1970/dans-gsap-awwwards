89ce8e41ad8e9c484e0142807c1290acdaf9e8bd
Daniel Wilkey <daniel@chancevoight.com>
2025-09-11 16:01:48 +1200

    implemented flavour section with ScrollSmoother

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
 import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavorSection";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+
   return (
     <main>
       <NavBar />
-      <HeroSection />
-      <MessageSection />
-      <div className="h-dvh border border-red-500" />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <div className="h-dvh border border-red-500" />
+        </div>
+      </div>
     </main>
   );
 };
